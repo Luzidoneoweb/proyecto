c371c56dbf2c837ce0f9dae1b87e6b0cd084e06f
Luz <Luz@idoneoweb.es>
2025-10-18 21:51:21 +0200

    main con login.progeso

--- php/pestanas/progreso.php
+++ php/pestanas/progreso.php
@@ -1,2 +1,2 @@
 <h2>Contenido de la Pestaña de Progreso</h2>
-<p>Aquí podrás ver tus estadísticas de aprendizaje, progreso en vocabulario y textos leídos.</p>
+<p> Aquí podrás ver tus estadísticas de aprendizaje, progreso en vocabulario y textos leídos.</p>
